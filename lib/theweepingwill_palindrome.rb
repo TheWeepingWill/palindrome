0393abae7d02836afeff3d4d05a99ba360af7e79
William Jones <williamseejones793@gmail.com>
2022-04-09 13:59:51 -0400

    Integer and modules

--- lib/theweepingwill_palindrome.rb
+++ lib/theweepingwill_palindrome.rb
@@ -2,9 +2,9 @@
 
 require_relative "theweepingwill_palindrome/version"
 
-#Adds proccessor and palindrome methods to the String class
-class String
 
+
+module TheweepingwillPalindrome
 #Returns true if a string is a palindrome, else it returns false
   def palindrome? 
     pc == pc.reverse
@@ -14,34 +14,21 @@ class String
   private
 #PC = processes content for 'palindrome?' usage
   def pc 
-    self.downcase.gsub(/\W+/, "")
+    self.to_s.downcase.gsub(/\W+/, "")
+  end
   end
 
-  
+#Adds proccessor and palindrome methods to the String class
+class String
+  include TheweepingwillPalindrome
 end
 
 #Adds proccessor and palindrome methods to the Integer class
 class Integer 
-#Returns true if a string of integers is a palindrome, else it returns false
-  def palindrome? 
-    pc == pc.reverse
-  end
-
-  private
-#PC = processed content. Downcases, and changes classes to string to be used 
-  def pc 
-    self.to_s.downcase
-  end
-
+  include TheweepingwillPalindrome
 end
 
 
 
-one = "racecar"
-two = "Able was I ere I saw Elba"
-three = "Madam, I'm Adam"
 
-p one.palindrome?
-p two.palindrome?
-p three.palindrome?
 
